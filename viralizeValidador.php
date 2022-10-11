3b97325147296c48100401300afa8f48b8e777c8
felipe <contatofelipevidalmendes@gmail.com>
2022-10-10 23:06:14 -0300

    inclusão nas expressões regulares de simbolos do ddd()

--- viralizeValidador.php
+++ viralizeValidador.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Plugin Name:       Viralize validador forms
+ * Plugin Name:       Viralize Validator Forms 
  * Plugin URI:        https://felipevidalmendes.com/
- * Description:       Validador de formularios viralize 
- * Version:           1.0.0
+ * Description:       Cria validações especificas nos campos dos formulários das LPs da Viralize 
+ * Version:           1.0.27
  * Author:            Felipe Vidal Mendes
  * Author URI:        https://felipevidalmendes.com/
  * License:           GPL
@@ -25,10 +25,15 @@ add_action( 'elementor_pro/forms/validation', function ( $record, $ajax_handler 
     }
 
     $field = current( $fields );
+    
 
-    if ( preg_match( '/[0-9]{2}-[0-9]{5}-[0-9]{4}/', $field['value'] ) !== 1 ) {
-        $ajax_handler->add_error( $field['id'], 'Please make sure the phone number is in XXX-XXX-XXXX format, eg: 123-456-7890' );
+    if ( preg_match( '/[0-9\(\\)\]{2} [0-9]{5}-[0-9]{4}/', $field['value'] ) !== 1 ) {
+        $ajax_handler->add_error( $field['id'], 'Por favor insira um número no formato  XX-XXXXX-XXXX, Exp: (12)34567-8900 ');
     }
+
+    // if ( preg_match( '/[0-9]{2}-[0-9]{5}-[0-9]{4}/', $field['value'] ) !== 1 ) {
+    //     $ajax_handler->add_error( $field['id'], 'Insira um número no formato  XX-XXXXX-XXXX Exp: (12)34567-8900' );
+    // }
 }, 10, 2 );
 
 
